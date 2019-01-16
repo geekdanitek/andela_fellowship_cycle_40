2ed42d8b21f0a60ab1d5b4f2f6e60af454ca6e9a
geekdanitek <adedejiayokunledaniel@gmail.com>
2019-01-16 12:07:51 +0100

    feature(database) create meetup database persist - create meetup and save data to database - unit testing for create meetup [Finishes: #163243931]

---
+++ server/models/dropTables.js
@@ -0,0 +1,22 @@
+import database from './databaseConnection';
+
+const dropTables = async () => {
+	try {
+		const query = `
+						DROP TABLE IF EXISTS users CASCADE;
+						DROP TABLE IF EXISTS meetups CASCADE;
+						DROP TABLE IF EXISTS questions CASCADE;
+						DROP TABLE IF EXISTS comments CASCADE;
+						DROP TABLE IF EXISTS rsvps CASCADE;
+						DROP TABLE IF EXISTS votes CASCADE;
+						`;
+		const response = await database.query(query);
+		await database.end();
+		return response;
+
+	} catch (errors) {
+		return errors;
+	}
+}
+
+dropTables();
